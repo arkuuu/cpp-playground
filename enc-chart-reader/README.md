b0241dd3207607e69dee30e55a3b2749d7cda54e
arkuuu <arkuuu@users.noreply.github.com>
2024-01-03 00:32:16 +0100

    Add enc-chart reader (work in progress)

---
+++ enc-chart-reader/README.md
@@ -0,0 +1,13 @@
+# ENC Chart Reader
+
+I try to read the contents of a S-57 enc chart from scratch.
+I use the first file of the free _Elbe-Luebeck-Kanal_ ENC chart for testing.
+It can be downloaded [here](https://www.elwis.de/DE/dynamisch/IENC/).
+
+The documentation for the S-57 standard can be found at:
+
+- <https://iho.int/uploads/user/pubs/standards/s-57/31Main.pdf>
+- <https://iho.int/uploads/user/pubs/standards/s-57/20ApB1.pdf>
+
+S-57 is based on the ISO/IEC 8211 standard.
+There is no free documentation for ISO/IEC 8211.
---
+++ enc-chart-reader/main.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct Leader
+{
+    int recordLength;
+    int baseAddressOfFieldArea;
+    int sizeOfFieldTagField;
+    int sizeOfFieldLengthField;
+    int sizeOfFieldPositionField;
+};
+
+struct DirectoryEntry
+{
+    std::string tag;
+    int length;
+    int position;
+};
+
+struct FieldControlField
+{
+    std::string content; // TODO split
+};
+
+struct DataDescriptiveField
+{
+    std::string content; // TODO split
+};
+
+struct DataField
+{
+    std::string content; // TODO split
+};
+
+struct DataDescriptiveRecord
+{
+    Leader leader;
+    std::vector<DirectoryEntry> directory;
+    FieldControlField fieldControlField;
+    std::vector<DataDescriptiveField> fieldArea;
+};
+
+struct DataRecord
+{
+    Leader leader;
+    std::vector<DirectoryEntry> directory;
+    std::vector<DataField> fieldArea;
+};
+
+struct EncData
+{
+    DataDescriptiveRecord ddr;
+    std::vector<DataRecord> drList;
+};
+
+std::string readStringFromFile(std::ifstream &file, const int length)
+{
+    std::vector<char> buffer(length);
+    file.read(&buffer[0], length);
+    std::string content(buffer.begin(), buffer.end());
+
+    return content;
+}
+
+std::string readStringFromFile(std::ifstream &file, const int startAdress, const int length)
+{
+    file.seekg(startAdress);
+
+    return readStringFromFile(file, length);
+}
+
+Leader readLeader(std::ifstream &file)
+{
+    Leader leader;
+
+    leader.recordLength = std::stoi(readStringFromFile(file, 5));
+    file.ignore(1); // Interchange level
+    file.ignore(1); // Leader identifier
+    file.ignore(1); // In line code extension indicator
+    file.ignore(1); // Version number
+    file.ignore(1); // Application indicator
+    file.ignore(2); // Field control length
+    leader.baseAddressOfFieldArea = std::stoi(readStringFromFile(file, 5));
+    file.ignore(3); // Extended character set indicator
+    leader.sizeOfFieldLengthField = std::stoi(readStringFromFile(file, 1));
+    leader.sizeOfFieldPositionField = std::stoi(readStringFromFile(file, 1));
+    file.ignore(1); // Reserved
+    leader.sizeOfFieldTagField = std::stoi(readStringFromFile(file, 1));
+
+    return leader;
+}
+
+std::vector<DirectoryEntry> readDirectory(std::ifstream &file, const Leader &leader)
+{
+    int currentAddress = file.tellg();
+    int sizeOfDirectory = leader.baseAddressOfFieldArea - currentAddress - 1;
+    int entrySize = leader.sizeOfFieldTagField + leader.sizeOfFieldLengthField + leader.sizeOfFieldPositionField;
+    int numberOfEntries = sizeOfDirectory / entrySize;
+
+    std::vector<DirectoryEntry> directory;
+
+    for (int i = 0; i < numberOfEntries; i++)
+    {
+        DirectoryEntry field;
+        field.tag = readStringFromFile(file, leader.sizeOfFieldTagField);
+        field.length = std::stoi(readStringFromFile(file, leader.sizeOfFieldLengthField));
+        field.position = std::stoi(readStringFromFile(file, leader.sizeOfFieldPositionField));
+        directory.push_back(field);
+    }
+
+    return directory;
+}
+
+FieldControlField readFieldControlField(
+    std::ifstream &file,
+    const Leader &leader,
+    std::vector<DirectoryEntry> &directory)
+{
+    DirectoryEntry firstEntry = directory[0];
+    FieldControlField fieldControlField;
+    fieldControlField.content = readStringFromFile(
+        file,
+        leader.baseAddressOfFieldArea + firstEntry.position,
+        firstEntry.length);
+    return fieldControlField;
+}
+
+std::vector<DataDescriptiveField> readDataDescriptiveFields(
+    std::ifstream &file,
+    const Leader &leader,
+    std::vector<DirectoryEntry> &directory)
+{
+    std::vector<DataDescriptiveField> fields;
+    for (const auto &directoryEntry : directory)
+    {
+        if (directoryEntry.tag == "0000")
+        {
+            // Tag "0000" is the special FieldControlField
+            continue;
+        }
+
+        DataDescriptiveField field;
+        field.content = readStringFromFile(
+            file,
+            leader.baseAddressOfFieldArea + directoryEntry.position,
+            directoryEntry.length);
+        fields.push_back(field);
+    }
+
+    return fields;
+}
+
+std::vector<DataField> readDataFields(
+    std::ifstream &file,
+    const Leader &leader,
+    std::vector<DirectoryEntry> &directory)
+{
+    std::vector<DataField> fields;
+    for (const auto &directoryEntry : directory)
+    {
+        DataField field;
+        field.content = readStringFromFile(
+            file,
+            leader.baseAddressOfFieldArea + directoryEntry.position,
+            directoryEntry.length);
+        fields.push_back(field);
+    }
+
+    return fields;
+}
+
+int main()
+{
+    std::ifstream encFile("Elbe-Luebeck-Kanal_2020-12-07/ENC_ROOT/1W7ELK00.000", std::ios::binary);
+    if (!encFile.is_open())
+    {
+        std::cerr << "ENC file not opened" << std::endl;
+        return 1;
+    }
+
+    EncData encData;
+
+    DataDescriptiveRecord ddr;
+    ddr.leader = readLeader(encFile);
+    ddr.directory = readDirectory(encFile, ddr.leader);
+    ddr.fieldControlField = readFieldControlField(encFile, ddr.leader, ddr.directory);
+    ddr.fieldArea = readDataDescriptiveFields(encFile, ddr.leader, ddr.directory);
+    encData.ddr = ddr;
+
+    // TODO dr's
+    DataRecord dr;
+    dr.leader = readLeader(encFile);
+    // TODO baseAddress is only relative to datarecord, not to file, so directory reading fails now
+    // dr.directory = readDirectory(encFile, dr.leader);
+    // dr.fieldArea = readDataFields(encFile, dr.leader, dr.directory);
+
+    std::cout << " -- Read bytes: " << encFile.tellg() << std::endl;
+    std::cout << std::endl;
+
+    return 0;
+}
